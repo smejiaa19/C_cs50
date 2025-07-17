53b86ff9eb0d22ac8f000a67b194505668a1f3a8
Silvio Mejia <silviomejiagarcia@gmail.com>
2025-07-17 11:58:10 -0600

    Iniciando el dia

--- C_pruebas/pruebas_personales/funciones.c
+++ C_pruebas/pruebas_personales/funciones.c
@@ -3,5 +3,19 @@
 int calculadora(int a, int b);
 int main(){
     int n1, n2, resp;
-    
+    printf("Ingrese dos numeros: ");
+    scanf("%i %i", &n1, &n2);
+
+    resp = calculadora(n1, n2);
+    printf("\nresp = %i\n", resp);
+
+    return 0;
 }
+
+int calculadora(int a, int b){
+    int result; 
+    result = a + b;
+    return result;
+}
+
+
