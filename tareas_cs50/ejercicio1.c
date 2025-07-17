513e5b1d05ccc6eadd91b195058a3354a88a8298
Silvio Mejia <silviomejiagarcia@gmail.com>
2025-07-16 23:26:46 -0600

    Commiteando lo que no meti antes, aun faltan terminar cosas

---
+++ tareas_cs50/ejercicio1.c
@@ -0,0 +1,11 @@
+// Declara un arreglo de enteros de 5 elementos y luego imprimirlo con un ciclo for. 
+
+#include <stdio.h>
+
+int main(){
+    int elementos[4] = 1, 2, 3, 4, 5;
+
+    for(int i = 1;){
+
+    }
+}
